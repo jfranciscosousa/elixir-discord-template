56715274458decda98396bd8a46f938b3671e0b4
Francisco Sousa <francisco.sousa@hey.com>
2021-01-17 13:31:41 +0000

    Add invite url to page

--- lib/elixir_discord_web/endpoint.ex
+++ lib/elixir_discord_web/endpoint.ex
@@ -1,15 +1,30 @@
 defmodule ElixirDiscordWeb.Endpoint do
   use Plug.Router
 
+  @template_dir "lib/elixir_discord_web/templates"
+  @invite_url "https://discord.com/oauth2/authorize?client_id=#{
+                System.get_env("DISCORD_CLIENT_ID")
+              }&scope=bot"
+
   plug(Plug.Logger)
   plug(:match)
   plug(:dispatch)
 
   get "/" do
-    send_resp(conn, 200, "Hello friend!")
+    render(conn, "index.html", invite_url: @invite_url)
   end
 
   match _ do
     send_resp(conn, 404, "")
+  end
+
+  defp render(%{status: status} = conn, template, assigns \\ []) do
+    body =
+      @template_dir
+      |> Path.join(template)
+      |> String.replace_suffix(".html", ".html.eex")
+      |> EEx.eval_file(assigns)
+
+    send_resp(conn, status || 200, body)
   end
 end
